2e286927eff8cfe2931e811c6e86d473c9d0106a
Christian Liebel <christian.liebel@thinktecture.com>
2023-10-19 11:32:26 +0200

    Lab #8

--- my-plugin/src/generators/my-generator/generator.ts
+++ my-plugin/src/generators/my-generator/generator.ts
@@ -1,5 +1,7 @@
 import {applicationGenerator} from "@nx/angular/generators";
+import {insertNgModuleImport} from "@nx/angular/src/generators/utils";
 import {generateFiles, joinPathFragments, readProjectConfiguration, Tree} from '@nx/devkit';
+import {insertImport} from "../utils/insert-import";
 import {MyGeneratorGeneratorSchema} from './schema';
 
 export async function myGeneratorGenerator(
@@ -17,6 +19,10 @@ export async function myGeneratorGenerator(
   const projectDir = readProjectConfiguration(tree, options.name).root;
   const destinationDir = joinPathFragments(projectDir, 'src', 'app');
   generateFiles(tree, sourceDir, destinationDir, {});
+
+  const modulePath = joinPathFragments(projectDir, 'src', 'app', 'app.module.ts');
+  insertImport(tree, modulePath, 'MyLibraryModule', '@my-workspace/my-library');
+  insertNgModuleImport(tree, modulePath, 'MyLibraryModule');
 }
 
 export default myGeneratorGenerator;
